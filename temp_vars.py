95df8d35f9cfe2f25b66870908297689c9651c92
liyang <yal009@ucsd.edu>
2022-06-16 14:33:55 +0800

    Added temp_vars.py to repo.

---
+++ temp_vars.py
@@ -0,0 +1,16 @@
+image_count = 0
+gt_cos_sim = 0.0
+pred_cos_sim = 0.0
+
+small_object_count = 0
+medium_object_count = 0
+large_object_count = 0
+small_object_success_count_25 = 0
+medium_object_success_count_25 = 0
+large_object_success_count_25 = 0
+small_object_success_count_50 = 0
+medium_object_success_count_50 = 0
+large_object_success_count_50 = 0
+small_object_success_count_75 = 0
+medium_object_success_count_75 = 0
+large_object_success_count_75 = 0
